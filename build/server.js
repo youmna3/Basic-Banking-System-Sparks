8c6f898eafe6e513d8e631dadf7ad353ddcd31b0
youmna3 <youmna54@gmail.com>
2023-06-04 17:43:13 +0300

    add loggar

--- build/server.js
+++ build/server.js
@@ -4,9 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const mongoose_1 = __importDefault(require("mongoose"));
+const config_1 = require("./config/config");
+// import morgan from "morgan";
+const Logging_1 = __importDefault(require("./library/Logging"));
 const app = (0, express_1.default)();
+// connect to mongo
+mongoose_1.default
+    .connect(config_1.config.mongo.url)
+    .then(() => {
+    Logging_1.default.info("connected to db");
+})
+    .catch((error) => {
+    console.log(error);
+});
+//app.use(morgan("dev"));
+// if (process.env.NODE_ENV == "development") {
+//   app.use(morgan("dev"));
+//   console.log(`mode ${process.env.NODE_ENV}`);
+// }
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,29 @@ import express from "express";
 import http from "http";
 import mongoose from "mongoose";
 import { config } from "./config/config";
+// import morgan from "morgan";
+//import Logging from "./library/Logging";
+import chalk from "chalk";
+import Logging from "./library/Logging";
 
 const app = express();
+
 // connect to mongo
 mongoose
   .connect(config.mongo.url)
   .then(() => {
-    console.log("connected to db");
+    Logging.info("connected to db");
   })
   .catch((error) => {
-    console.log(error);
+    Logging.error(error);
   });
-
+//app.use(morgan("dev"));
+// if (process.env.NODE_ENV == "development") {
+//   app.use(morgan("dev"));
+//   console.log(`mode ${process.env.NODE_ENV}`);
+// }
 const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
 });
