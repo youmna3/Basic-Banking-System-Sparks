9187bd8686877d48f8f4e60f48f8a27daf3bf0a9
youmna3 <youmna54@gmail.com>
2023-06-07 21:50:10 +0300

    adding customer model, controller and routes

---
+++ src/models/Customer.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ICustomer {
+  name: string;
+  email: string;
+  account_number: string;
+  balance: number;
+}
+
+export interface ICustomerModel extends ICustomer, Document {}
+
+const CustomerSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    account_number: { type: String, required: true, unique: true },
+    balance: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+export default mongoose.model<ICustomerModel>("Customer", CustomerSchema);
